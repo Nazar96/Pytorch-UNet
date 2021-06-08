96513cd7c86eac06452eddfc91ead1d0beeb715c
Тёхта Назарий Александрович <Tekhta.N.A@sberbank.ru>
2021-06-08 20:58:40 +0300

    Added dataloader

---
+++ utils/dataloader.py
@@ -0,0 +1,92 @@
+import os
+import albumentations as A
+import cv2
+from sklearn.model_selection import train_test_split
+from .dataset import TableMaskDataset
+from torch.utils.data import DataLoader
+
+img_pad_size = 1024
+img_size = 512
+val_size = 50
+batch_size = 16
+
+pixel_aug = [
+    A.Perspective(p=0.25),
+    A.GlassBlur(p=0.1),
+    A.RandomGridShuffle(p=0.15),
+    A.RandomRain(rain_type='heavy', p=0.15),
+    A.RandomFog(fog_coef_lower=0.1, fog_coef_upper=0.5, p=0.15),
+    A.RandomSunFlare(src_radius=200, num_flare_circles_lower=1, p=0.25),
+    A.OpticalDistortion(distort_limit=0.15, shift_limit=0, p=0.25),
+]
+
+pad_aug = [
+    A.LongestMaxSize(img_pad_size, p=0.25),
+
+    A.OneOf([
+        A.PadIfNeeded(
+            min_height=img_pad_size,
+            min_width=img_pad_size,
+            border_mode=cv2.BORDER_CONSTANT,
+            value=(255, 255, 255),
+            mask_value=(255, 255, 255),
+        ),
+        A.PadIfNeeded(
+            min_height=img_pad_size,
+            min_width=img_pad_size,
+            border_mode=cv2.BORDER_REFLECT,
+        )], p=1,
+    ),
+    A.RandomCrop(width=img_size, height=img_size, p=1),
+]
+
+back_aug = [
+    A.ShiftScaleRotate(0, 1, rotate_limit=180, p=0.75),
+    A.PadIfNeeded(
+        min_height=img_pad_size,
+        min_width=img_pad_size,
+        border_mode=cv2.BORDER_REFLECT,
+    ),
+    A.RandomCrop(width=img_size, height=img_size, p=1),
+]
+
+affin_aug = [
+    A.HorizontalFlip(p=0.5),
+    A.VerticalFlip(p=0.5),
+    A.ShiftScaleRotate(0.5, 0.5, rotate_limit=5, p=0.5),
+    A.RandomRotate90(p=0.5),
+]
+
+aug_list = pixel_aug + pad_aug + affin_aug
+aug_back_list = pixel_aug + back_aug
+
+train_alb_transform = A.Compose(aug_list)
+val_alb_transform = A.Compose(pad_aug)
+back_alb_transform = A.Compose(aug_back_list)
+
+
+def get_dataloader(
+        img_path='./test/type_1/',
+        mask_path='./test/type_2/',
+        background_path='./background/',
+):
+    labels = os.listdir(img_path)
+    backgrounds = os.listdir(background_path)
+
+    train_labels, val_labels = train_test_split(labels, test_size=val_size, random_state=228)
+
+    train_ds = TableMaskDataset(
+        img_path, mask_path, background_path, train_labels,
+        transform=train_alb_transform, background_transform=back_alb_transform, back_labels=backgrounds,
+        back_proba=0.75,
+    )
+    val_ds = TableMaskDataset(
+        img_path, mask_path, background_path, val_labels,
+        transform=val_alb_transform, background_transform=back_alb_transform, back_labels=backgrounds,
+        back_proba=0.75,
+    )
+
+    train_dataloader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_ds, batch_size=val_size, shuffle=False)
+
+    return train_dataloader, val_dataloader
--- utils/dataset.py
+++ utils/dataset.py
@@ -1,11 +1,14 @@
 from os.path import splitext
 from os import listdir
-import numpy as np
 from glob import glob
 import torch
 from torch.utils.data import Dataset
 import logging
 from PIL import Image
+import cv2
+from random import choice
+import numpy as np
+import os
 
 
 class BasicDataset(Dataset):
@@ -69,3 +72,64 @@ class BasicDataset(Dataset):
 class CarvanaDataset(BasicDataset):
     def __init__(self, imgs_dir, masks_dir, scale=1):
         super().__init__(imgs_dir, masks_dir, scale, mask_suffix='_mask')
+
+
+class TableMaskDataset(Dataset):
+    def __init__(self, img_dir, mask_dir, background_dir=None, labels=None, transform=None, background_transform=None,
+                 back_labels=None, back_proba=0.5, empty_proba=0.05):
+        self.img_labels = labels
+        self.img_dir = img_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.background_dir = background_dir
+        self.background_transform = background_transform
+        self.back_labels = back_labels
+        self.sigma = 3
+        self.empty_proba = empty_proba
+        self.back_proba = back_proba
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels[idx])
+        mask_path = os.path.join(self.mask_dir, self.img_labels[idx])
+
+        is_empty = np.random.binomial(1, self.empty_proba)
+        is_background = np.random.binomial(1, self.back_proba)
+
+        if is_empty:
+            img_path = mask_path
+
+        image = cv2.imread(img_path)
+        mask = cv2.imread(mask_path)
+        mask = cv2.GaussianBlur(mask, (15, 15), self.sigma)
+
+        if self.transform is not None:
+            transformed = self.transform(image=image, mask=mask)
+            image = transformed["image"]
+            mask = transformed["mask"]
+
+        if (self.background_transform is not None) and (is_background):
+            image = self._add_background(image)
+
+        image = self._postproc(image)
+        mask = self._postproc(mask)
+
+        return (image, mask)
+
+    @staticmethod
+    def _postproc(x):
+        x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
+        x = cv2.normalize(x, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        x = np.expand_dims(x, axis=0)
+        return x
+
+    def _add_background(self, img):
+        back_rand = choice(self.back_labels)
+        back_path = os.path.join(self.background_dir, back_rand)
+        back = cv2.imread(back_path)
+        back = self.background_transform(back=back)['back']
+        result = np.mean([back, img], axis=0).astype(np.uint8)
+        return result
+
